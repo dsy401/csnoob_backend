f9f46f7b611c6a78edd73cf740baf5d017fc573a
oliver <oliverdeng1005@hotmail.com>
2019-08-27 16:23:02 +1200

    update

--- api/controllers/course.js
+++ api/controllers/course.js
@@ -6,7 +6,10 @@ exports.course_get_all = (req,res,next) =>{
     const result = new Result()
     course.find().exec()
         .then(docs=>{
-            result.Data = docs
+            result.Data = docs.map(s=>{
+                const stage = Number(s.code.split(' ')[1][0])
+                return {_id:s._id,code:s.code,name:s.name,description:s.description,school:s.school,type:s.type,stage:stage}
+            })
             res.status(200).json(result)
         })
         .catch(err=>{
