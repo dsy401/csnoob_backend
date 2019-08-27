6dd39d9e434beb5f36fb41d48dfe73a92e25c8f0
oliver <oliverdeng1005@hotmail.com>
2019-08-28 00:46:06 +1200

    update

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-const http = require('http')
-const app = require('./app');
+// const http = require('http')
+// const app = require('./app');
+//
+// const port = process.env.PORT || 3000;
+//
+// const server = http.createServer(app);
+//
+// server.listen(port)
+
+const fs = require('fs')
+const https = require('https')
+const app =require('./app')
+
+//ssl
+const privateKey = fs.readFileSync('./path/to/private.pem','utf8')
+const certificate = fs.readFileSync('./path/to/file.crt','utf8')
+const credentials = {key:privateKey,cert:certificate}
+//
 
 const port = process.env.PORT || 3000;
-
-const server = http.createServer(app);
-
+const server = https.createServer(credentials,app);
 server.listen(port)
