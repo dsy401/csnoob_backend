f7f145c983b100b9193e048d42e03fdc8181c779
oliver <oliverdeng1005@hotmail.com>
2019-08-27 20:51:32 +1200

    update

--- api/models/teacher.js
+++ api/models/teacher.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const teacherSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: {type: String, required:true},
+    title:{type:String,required:true},
     courses: [{type: mongoose.Schema.Types.ObjectId,ref:'course'}],
     school: {type: Number, required: true},
 
