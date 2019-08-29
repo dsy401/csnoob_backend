1e43848015dc12f834630ae13572c0eeea6ccb53
oliver <oliverdeng1005@hotmail.com>
2019-08-29 15:25:09 +1200

    update

--- api/models/thread.js
+++ api/models/thread.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const threadSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
+    author: {type:String,required:true},
     title: {type: String,required:true},
     content: {type: String,required: true},
     forumId: {type: mongoose.Schema.Types.ObjectId}
