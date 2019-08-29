cb0cc5b2ee23dc2ffdf9c5198d9c1a423a52e878
oliver <oliverdeng1005@hotmail.com>
2019-08-29 19:13:59 +1200

    update

--- api/controllers/thread.js
+++ api/controllers/thread.js
@@ -2,12 +2,25 @@ const mongoose = require('mongoose');
 const thread = require('../models/thread')
 const {Result} = require('../models/result')
 
-exports.get_all_threads_by_forumId = (req,res,next) =>{
+exports.get_all_threads_by_forumId = async (req,res,next) =>{
     const result = new Result();
-    thread.find({'forumId':req.params.forumId})
+    const pageNum = Number(req.params.PageNum)
+    if (isNaN(pageNum) == true || pageNum <= 0){
+        result.IsSuccess =false;
+        result.ErrorMessage = "Page number is not a integer or zero or negative integer"
+        return res.status(200).json(result);
+    }
+    const total = await thread
+        .count({'forumId':req.params.forumId})
+
+    const totalPageNum = await Math.floor(total/10) + 1
+    thread
+        .find({'forumId':req.params.forumId})
+        .limit(10)
+        .skip((pageNum-1)*10)
         .exec()
         .then(docs=>{
-            result.Data = docs;
+            result.Data = {total: totalPageNum, currentPage:pageNum, details:docs};
             res.status(200).json(result)
         })
         .catch(err=>{
@@ -15,4 +28,4 @@ exports.get_all_threads_by_forumId = (req,res,next) =>{
             result.ErrorMessage = err;
             res.status(500).json(result)
         })
-}
+};
--- api/routes/thread.js
+++ api/routes/thread.js
@@ -3,6 +3,6 @@ const threadController = require('../controllers/thread');
 const router = express.Router()
 
 
-router.get('/:forumId',threadController.get_all_threads_by_forumId);
+router.get('/:forumId/:PageNum',threadController.get_all_threads_by_forumId);
 
 module.exports = router;
