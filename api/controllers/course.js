b26b8038d3c72c9be8d539b38ae96189b0516e9d
oliver <oliverdeng1005@hotmail.com>
2019-08-31 01:46:28 +1200

    update

--- api/controllers/course.js
+++ api/controllers/course.js
@@ -40,6 +40,22 @@ exports.get_courses_by_code = (req,res,next) => {
             result.ErrorMessage = err
             res.status(500).json(result);
         });
+};
+
+exports.get_course_by_courseId = (req,res,next) =>{
+    const result = new Result();
+    course.findOne({"_id":req.params.courseId})
+        .exec()
+        .then(s=>{
+            const stage = Number(s.code.split(' ')[1][0])
+            result.Data = {_id:s._id,code:s.code,name:s.name,description:s.description,school:s.school,type:s.type,stage:stage}
+            res.status(200).json(result)
+        })
+        .catch(err=>{
+            result.IsSuccess = false
+            result.ErrorMessage = err
+            res.status(500).json(result);
+        });
 }
 
 
