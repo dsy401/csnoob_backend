d9053df92a416622c0436e03e0d35714d980bc56
oliver <oliverdeng1005@hotmail.com>
2019-09-02 14:38:54 +1200

    update

--- api/controllers/threadComment.js
+++ api/controllers/threadComment.js
@@ -16,4 +16,26 @@ exports.get_threadComment_by_threadId = (req,res,next) =>{
             result.ErrorMessage = err;
             res.status(500).json(result)
         })
-}
+};
+
+
+exports.post_one_thread_comment = (req,res,next) => {
+    const result = new Result();
+    const threadCommentModel = new threadComment({
+        _id: new mongoose.Types.ObjectId(),
+        comment: req.body.comment,
+        name: req.body.name,
+        threadId: req.body.threadId
+    });
+
+    threadCommentModel.save()
+        .then(doc=>{
+            result.Data = "comment successfully";
+            res.status(201).json(result)
+        })
+        .catch(err=>{
+            result.IsSuccess = false;
+            result.ErrorMessage = err;
+            res.status(500).json(result)
+        })
+};
--- api/routes/threadComment.js
+++ api/routes/threadComment.js
@@ -3,6 +3,6 @@ const threadCommentController = require('../controllers/threadComment');
 const router = express.Router()
 
 router.get('/getThreadCommentByThreadId/:threadId',threadCommentController.get_threadComment_by_threadId);
-
+router.post('/',threadCommentController.post_one_thread_comment)
 
 module.exports = router;
