53df22e7ac4c4cf612745d8d61e43a1ec3936e13
oliver <oliverdeng1005@hotmail.com>
2019-09-05 12:10:44 +1200

    update

--- api/controllers/teacher.js
+++ api/controllers/teacher.js
@@ -3,6 +3,49 @@ const teacher = require('../models/teacher');
 const {Result} = require('../models/result')
 const {rateCounter} = require('../../util/rateCounter')
 
+
+exports.get_teachers_by_schoolNum = (req,res,next)=>{
+    const result = new Result()
+    teacher
+        .aggregate([
+            {
+                $lookup: {
+                    from: 'teacherRating',
+                    localField: '_id',
+                    foreignField: 'teacherId',
+                    as: 'rates'
+                },
+            },
+            {
+              $match: {
+                  "school": Number(req.params.schoolNum)
+              }
+            },
+            {
+                $lookup: {
+                    from: 'course',
+                    localField: 'courses',
+                    foreignField: '_id',
+                    as: 'courses'
+                }
+            }
+        ])
+        .exec()
+        .then(doc=>{
+            result.Data = doc.map(s=>{
+                return {_id:s._id,name:s.name,title:s.title,courses:s.courses,school:s.school,rate: rateCounter(s.rates)}
+            });
+            res.status(200).json(result)
+        })
+        .catch(err=>{
+            result.IsSuccess = false;
+            result.ErrorMessage = err;
+            res.status(500).json(result)
+        })
+
+}
+
+
 exports.get_all_teacher = (req,res,next)=>{
     const result = new Result()
     teacher
--- api/routes/teacher.js
+++ api/routes/teacher.js
@@ -5,4 +5,5 @@ const router = express.Router();
 
 router.get('/',teacherController.get_all_teacher)
 router.get('/:name',teacherController.get_teachers_by_name)
+router.get('/getTeachersBySchoolNum/:schoolNum',teacherController.get_teachers_by_schoolNum)
 module.exports = router;
